7a819ae50cd205e45320971aa31abd7500607638
m3rlin45 <m3rlin45@gmail.com>
2015-03-01 17:17:23 -0700

    spell checking now works, this script will return an error if it fails

--- resumeSpellCheck.js
+++ resumeSpellCheck.js
@@ -1,5 +1,51 @@
 var fs = require('fs');
-var resume = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
-resume.forEach(function (resumeObj) {
-    console.log(resumeObj);
+var moment = require('moment');
+var validUrl = require('valid-url');
+var unique = require('unique-words');
+var spelling = require('spelling');
+var dictionary = require('spelling/dictionaries/en_US.js');
+var dict = new spelling(dictionary);
+
+//add our additional words to dictionary
+var safeWords = JSON.parse(fs.readFileSync('dictionaryAdditions.json', 'utf8'));
+safeWords.AllowedWords.forEach(function (word) {
+    dict.insert(word)
 });
+
+var resume = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
+
+var checkableStrings = [];
+
+
+function findCheckableStrings(aResume){
+    var keys = Object.keys(aResume);
+    for (var i = 0; i < keys.length; i++) {
+        if (typeof aResume[keys[i]] === "object") {
+            findCheckableStrings(aResume[keys[i]]);
+        }
+        else {
+            if(typeof aResume[keys[i]] === "string") 
+            {
+                //next get rid of all dates and urls, cant spell check those
+                if (!(moment(aResume[keys[i]]).isValid()||validUrl.isUri(aResume[keys[i]]))) {
+                    checkableStrings.push(aResume[keys[i]]);
+                }
+            }
+        }
+    }
+}
+
+findCheckableStrings(resume);
+
+//convert to only unique words
+var checkableWords = unique(checkableStrings);
+//make a list of words that are misspelled
+var mispelledWords = checkableWords.filter(function (word) {
+    return !dict.lookup(word).found;
+});
+
+if (mispelledWords.length != 0) {
+    console.log("Mispelled words found, either fix them or add them to dictionaryAdditions.json");
+    console.log(mispelledWords);
+    process.exit(1)
+}
