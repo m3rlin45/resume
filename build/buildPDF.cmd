a00c0806719d95d1f5ef8d2654132b7c56392110
Chris Dewan <m3rlin45@gmail.com>
2019-03-30 14:29:39 -0700

    Fix pdf generation

--- build/buildPDF.cmd
+++ build/buildPDF.cmd
@@ -5,23 +5,23 @@ REM https://github.com/shanselman/march-is-for-makers/blob/master/getruby.cmd
 
 REM Put Tex in Path
 REM You can also use %TEMP% but it is cleared on site restart. Tools is persistent.
-SET PATH=%PATH%;D:\home\site\deployments\tools\t\miktex-2.9.5105\miktex\bin
+SET PATH=%PATH%;D:\home\site\deployments\tools\t\miktex-portable\miktex\bin
 
 REM I am in the repository folder
 pushd D:\home\site\deployments\tools 
 if not exist t md t
 cd t
-if exist miktex-2.9.5105 goto end
+if exist miktex-portable goto end
 
 echo No LaTeX, need to get it!
 
 REM Get Ruby and Rails
 REM 32bit
-REM  get miktex portable from http://mirror.jmu.edu/pub/CTAN/systems/win32/miktex/setup/miktex-portable-2.9.5105.exe
-curl -o miktex-2.9.5105.exe http://mirror.jmu.edu/pub/CTAN/systems/win32/miktex/setup/miktex-portable-2.9.5105.exe
+REM  get miktex portable from https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x86/miktex-portable-2.9.6942.exe
+curl -o miktex-portable.exe https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x86/miktex-portable-2.9.6942.exe
 REM Azure puts 7zip here!
 EcHO START Unzipping MikTex
-d:\7zip\7za x -y -omiktex-2.9.5105 miktex-2.9.5105.exe > outmiktex
+d:\7zip\7za x -y -omiktex-portable miktex-portable> outmiktex
 EcHO DONE Unzipping MikTex
 
 
