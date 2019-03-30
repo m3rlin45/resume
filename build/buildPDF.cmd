51bfeb1b98ce95b0b4246cfb7a58ddc7b0d1fe44
Chris Dewan <m3rlin45@gmail.com>
2019-03-30 15:03:37 -0700

    remove incorrect comments and trigger deployment

--- build/buildPDF.cmd
+++ build/buildPDF.cmd
@@ -15,8 +15,6 @@ if exist miktex-portable goto end
 
 echo No LaTeX, need to get it!
 
-REM Get Ruby and Rails
-REM 32bit
 REM  get miktex portable from https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x86/miktex-portable-2.9.6942.exe
 curl -L -o miktex-portable.exe https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x86/miktex-portable-2.9.6942.exe
 REM Azure puts 7zip here!
