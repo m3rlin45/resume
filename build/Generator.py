026a514f00f63f37a206ba3a23115c9e37759af5
Chris Dewan <chris.dewan@m3rlin.net>
2021-10-12 23:04:02 -0700

    Latex Generation Works

--- build/Generator.py
+++ build/Generator.py
@@ -21,6 +21,26 @@ def generate_html(json_resume, template_path, output_file):
     add_filters(env)
     env.get_template(template_path).stream(resume_data).dump(output_file)
 
+
+@cli.command()
+@click.argument("json_resume", type=click.File("r"))
+@click.argument("template_path")
+@click.argument("output_file", type=click.File("w"))
+def generate_latex(json_resume, template_path, output_file):
+    resume_data = json.load(json_resume)
+
+    env = Environment(
+        loader=FileSystemLoader("./"),
+        autoescape=False,
+        block_start_string="<%",
+        block_end_string="%>",
+        variable_start_string="<$",
+        variable_end_string="$>",
+        comment_start_string="<#",
+        comment_end_string="#>",
+    )
+    add_filters(env)
+    env.get_template(template_path).stream(resume_data).dump(output_file)
     
 if __name__ == "__main__":
     cli()
