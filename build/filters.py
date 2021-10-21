d84f1d359abef9462574fe86379597a82268a15f
Chris Dewan <chris.dewan@m3rlin.net>
2021-10-20 20:32:52 -0700

    Added black and reformatted all .py files

--- build/filters.py
+++ build/filters.py
@@ -2,9 +2,11 @@ from collections import defaultdict
 from datetime import date
 from pylatex.utils import escape_latex
 
+
 def add_filters(env):
     def DateYearMonthFilter(datestr):
         return date.fromisoformat(datestr).strftime("%B %Y")
+
     env.filters["DateYearMonth"] = DateYearMonthFilter
 
     def ConvertSkillsFilter(skills):
@@ -12,9 +14,13 @@ def add_filters(env):
         for skill in skills:
             new_skills[skill["name"]].append(skill)
 
-        return [{"name": name, "entries": entries} for name, entries in new_skills.items()]
+        return [
+            {"name": name, "entries": entries} for name, entries in new_skills.items()
+        ]
+
     env.filters["ConvertSkills"] = ConvertSkillsFilter
-                
+
     def TeXEscapeFilter(input):
         return escape_latex(input)
+
     env.filters["TeXEscape"] = TeXEscapeFilter
--- test/spell_check_test.py
+++ test/spell_check_test.py
@@ -7,7 +7,7 @@ import validators
 from spellchecker import SpellChecker
 
 spell = SpellChecker()
-ROOT_PATH = os.path.join(os.path.dirname(__file__),"../")
+ROOT_PATH = os.path.join(os.path.dirname(__file__), "../")
 USER_DICT_PATH = os.path.join(ROOT_PATH, "dictionaryAdditions.json")
 number_checker = re.compile(r"[\d]+")
 
@@ -15,24 +15,25 @@ number_checker = re.compile(r"[\d]+")
 def normalize(word):
     return word.lower().translate(str.maketrans("", "", string.punctuation))
 
+
 def get_allowed_words():
     with open(USER_DICT_PATH) as custom_words:
         return json.load(custom_words)["AllowedWords"]
-    
+
 
 class TestSpelling(unittest.TestCase):
-
     def test_user_dict_sorted(self):
         self.assertListEqual(get_allowed_words(), sorted(get_allowed_words()))
-        
 
     def test_spelling(self):
-    
-        with open(os.path.join(ROOT_PATH,"resume.json")) as resume_json:
+
+        with open(os.path.join(ROOT_PATH, "resume.json")) as resume_json:
             resume = json.load(resume_json)
 
-        spell.word_frequency.load_words([normalize(word) for word in get_allowed_words()])
-        
+        spell.word_frequency.load_words(
+            [normalize(word) for word in get_allowed_words()]
+        )
+
         def check_spelling(obj):
             if not obj:
                 return
@@ -46,12 +47,13 @@ class TestSpelling(unittest.TestCase):
                     normalized = normalize(piece)
                     if number_checker.match(normalized):
                         continue
-                    assert spell[normalized], f"spelling error: {piece}: normalized as {normalized}"
+                    assert spell[
+                        normalized
+                    ], f"spelling error: {piece}: normalized as {normalized}"
             elif isinstance(obj, list):
                 [check_spelling(child) for child in obj]
             else:
                 assert isinstance(obj, dict)
                 [check_spelling(child) for child in obj.values()]
 
-        check_spelling(resume)        
-    
+        check_spelling(resume)
